94f1cf6bb17111781ba5532a61d5ce1bfbbb5674
Murilo Leonardo <155448925+MuriloLeo@users.noreply.github.com>
2024-05-31 15:50:45 -0300

    Arrumei os territórios

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Expandir o menu
+//Expandir o menu lateral
 
 const btnExpandir = document.querySelector('#btn-exp')
 const menuLateral = document.querySelector ('.menu-lateral')
@@ -6,3 +6,23 @@ const menuLateral = document.querySelector ('.menu-lateral')
 btnExpandir.addEventListener('click', function(){
     menuLateral.classList.toggle('expandir')
 })
+
+//Mostrar mais territórios
+
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+})
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add('remover');
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
